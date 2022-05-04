274c65bb2f3ed8e9bee4922123a28309b2832a85
D3ni53 <denise.meyer@capgemini.com>
2022-05-04 15:26:13 +0200

    rafactor

--- cypress/support/step_definition/news/readNews.spec.js
+++ cypress/support/step_definition/news/readNews.spec.js
@@ -1,22 +1,20 @@
 import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps'
 import Read_News from '../../page/news/readNewsPageObject'
 
+const selectReadNews= new Read_News()
+
 When('I go to the school news on the dashboard', () => {
-    const selectReadNews= new Read_News()
     selectReadNews.goToNewsOnDashboard()
 })
 
 When('I go to the team news on the dashboard', () => {
-    const selectReadNews= new Read_News()
     selectReadNews.goToTeamNewsOnDashboard()
 })
 
 Then('I can read the school news', () => {
-    const selectReadNews= new Read_News()
     selectReadNews.readSchoolNews()
 })
 
 Then('I can read the team news', () => {
-    const selectReadNews= new Read_News()
     selectReadNews.readTeamNews()
 })
