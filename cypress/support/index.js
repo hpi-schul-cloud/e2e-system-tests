47a3733a9a4b52d895a5ccf24be44bdd71a8165e
Uzaeir <uzair.khan@dataport.de>
2022-05-21 22:25:47 +0200

    initial commit

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -16,8 +16,13 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 import './custom_commands/login'
+import addContext from 'mochawesome/addContext'
 
-
-
+Cypress.on('test:after:run', (test, runnable) => {
+  if (test.state === 'failed') {
+    const screenshot = `assets/${Cypress.spec.name}/${runnable.parent.title} -- ${test.title} (failed).png`
+    addContext({ test }, screenshot)
+  }
+})
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
