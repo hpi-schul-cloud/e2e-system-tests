fddaf2478d9b931645d426b867472720471dd7bd
Uzaeir <uzair.khan@dataport.de>
2022-06-02 14:27:31 +0200

    initial commit

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -24,7 +24,7 @@ const allureWriter = require('@shelex/cypress-allure-plugin/writer')
 const cucumber = require('cypress-cucumber-preprocessor').default
 
 module.exports = (on, config) => {
-  allureWriter(on, config)
   on('file:preprocessor', cucumber())
+  allureWriter(on, config)
   return config
 }
