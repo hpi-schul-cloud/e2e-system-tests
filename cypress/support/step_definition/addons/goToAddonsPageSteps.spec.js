cebf95afdd60cfa25810405406aa454bd80f7e8a
atlldwp <an2478288@gmail.com>
2022-06-07 12:23:25 +0200

    test update

--- cypress/support/step_definition/addons/goToAddonsPageSteps.spec.js
+++ cypress/support/step_definition/addons/goToAddonsPageSteps.spec.js
@@ -1,1 +1,9 @@
+/*
+Scenario: Check navigation in sidebar as admin
 
+Given ('I am logged in as a 'admin' at 'brb'')
+steps defined --> \step_definition\global_login_logout\loginLogoutSteps.spec.js
+
+When I go to AddOns overview
+stesp defined --> \step_definition\addons\commonAddonsRelatedSteps.spec.js
+*/
