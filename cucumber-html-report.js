f217b6e520ac8ae1b1f4d61f2b8ebc31532eb03f
Uzaeir <uzair.khan@dataport.de>
2022-06-17 13:29:27 +0200

    removing continue-on-error

--- cucumber-html-report.js
+++ cucumber-html-report.js
@@ -1,5 +1,133 @@
 const report = require('multiple-cucumber-html-reporter')
 const fs = require('fs')
+const fs_extra = require('fs-extra')
+const path = require('path')
+
+const cucumberJsonDir = path.resolve(process.cwd(), 'cypress/cucumber-json')
+/*const cucumberReportFileMap = {}
+const cucumberReportMap = {}
+const jsonIndentLevel = 2*/
+const htmlReportDir = path.resolve(process.cwd(), 'cypress/reports')
+/*const screenshotsDir = path.resolve(process.cwd(), 'cypress/screenshots')
+
+getCucumberReportMaps()
+addScreenshots()
+generateReport()
+
+function getCucumberReportMaps () {
+  filenames = fs_extra.readdirSync(cucumberJsonDir)
+  const files = fs_extra.readdirSync(cucumberJsonDir).filter(file => {
+    return file.indexOf('.json') > -1
+  })
+  files.forEach(file => {
+    const json = JSON.parse(
+      fs_extra.readFileSync(path.join(cucumberJsonDir, file))
+    )
+    if (!json[0]) {
+      return
+    }
+    //console.log(json)
+    const [feature] = json[0].uri.split('/').reverse()
+    cucumberReportFileMap[feature] = file
+    cucumberReportMap[feature] = json
+    console.log(cucumberReportMap[feature][0])
+  })
+}
+
+function addScreenshots () {
+  if (fs_extra.existsSync(screenshotsDir)) {
+    //only if screenshots exists
+    const prependPathSegment = pathSegment => location =>
+      path.join(pathSegment, location)
+
+    const readdirPreserveRelativePath = location =>
+      fs_extra.readdirSync(location).map(prependPathSegment(location))
+
+    const readdirRecursive = location =>
+      readdirPreserveRelativePath(location).reduce(
+        (result, currentValue) =>
+          fs_extra.statSync(currentValue).isDirectory()
+            ? result.concat(readdirRecursive(currentValue))
+            : result.concat(currentValue),
+        []
+      )
+
+    const screenshots = readdirRecursive(path.resolve(screenshotsDir)).filter(
+      file => {
+        return file.indexOf('.png') > -1
+      }
+    )
+
+    const featuresList = Array.from(
+      new Set(screenshots.map(x => x.match(/[\w-_.]+.feature/g)[0]))
+    )
+
+    featuresList.forEach(feature => {
+      screenshots.forEach(screenshot => {
+        const regex = /(?<=--\ ).+?((?=\ (example\ #\d+))|(?=\ (failed))|(?=.\w{3}))/g
+        const [scenarioName] = screenshot.match(regex)
+
+        var filename = screenshot.replace(/^.*[\\\/]/, '')
+
+        const featureSelected = cucumberReportMap[feature][0]
+
+        let myScenarios = []
+
+        cucumberReportMap[feature][0].forEach(item => {
+          console.log(item)
+          let fullFileName = featureSelected.name + ' -- ' + item.name
+          if (filename.includes(fullFileName)) {
+            myScenarios.push(item)
+          }
+        })
+
+        if (!myScenarios) {
+          return
+        }
+        let foundFailedStep = false
+        myScenarios.forEach(myScenario => {
+          if (foundFailedStep) {
+            return
+          }
+          let myStep
+          if (screenshot.includes('(failed)')) {
+            myStep = myScenario.steps.find(
+              step => step.result.status === 'failed'
+            )
+          } else {
+            myStep = myScenario.steps.find(
+              step => step.result.status === 'passed'
+            )
+          }
+          if (!myStep) {
+            return
+          }
+          const data = fs_extra.readFileSync(path.resolve(screenshot))
+          if (data) {
+            const base64Image = Buffer.from(data, 'binary').toString('base64')
+            if (!myStep.embeddings) {
+              myStep.embeddings = []
+              myStep.embeddings.push({
+                data: base64Image,
+                mime_type: 'image/png',
+                name: myStep.name
+              })
+              foundFailedStep = true
+            }
+          }
+        })
+        //Write JSON with screenshot back to report file.
+        fs_extra.writeFileSync(
+          path.join(cucumberJsonDir, cucumberReportFileMap[feature]),
+          JSON.stringify(cucumberReportMap[feature], null, jsonIndentLevel)
+        )
+      })
+    })
+  }
+}
+*/
+
+generateReport()
 
 let osMap = os => {
   if (os.startsWith('win')) {
@@ -31,52 +159,56 @@ let browserMap = browser => {
   }
 }
 
-fs.readFile('cypress/.run/results.json', function read (err, data) {
-  if (err) {
-    throw err
-  }
-  let runInfos = JSON.parse(data)
-  report.generate({
-    jsonDir: 'cypress/cucumber-json',
-    reportPath: 'cypress/reports',
-    openReportInBrowser: false,
-    saveCollectedJSON: true,
-    pageTitle: 'dBildungscloud E2E Test Report',
-    reportName:
-      'E2E Cucumber Test Report ' +
-      new Date(runInfos.startedTestsAt).toLocaleString(),
-    pageFooter: ' ',
-    hideMetadata: false,
-    displayReportTime: true,
-    metadata: {
-      browser: {
-        name: browserMap(runInfos.browserName),
-        version: runInfos.browserVersion
-      },
-      device: 'Cypress',
-      platform: {
-        name: osMap(runInfos.osName)
+function generateReport () {
+  if (!fs_extra.existsSync(cucumberJsonDir)) {
+    console.warn('REPORT CANNOT BE CREATED!')
+  } else {
+    fs.readFile('cypress/.run/results.json', function read (err, data) {
+      if (err) {
+        throw err
       }
-    },
-    customData: {
-      title: 'Run info',
-      data: [
-        { label: 'Project', value: 'dBildungscloud' },
-        {
-          label: 'Instance',
-          value:
-            runInfos.config.env.BRB ||
-            runInfos.config.env.NBC
+      let runInfos = JSON.parse(data)
+      report.generate({
+        jsonDir: cucumberJsonDir,
+        reportPath: htmlReportDir,
+        openReportInBrowser: false,
+        saveCollectedJSON: true,
+        pageTitle: 'dBildungscloud E2E Test Report',
+        reportName:
+          'E2E Cucumber Test Report ' +
+          new Date(runInfos.startedTestsAt).toLocaleString(),
+        pageFooter: ' ',
+        hideMetadata: false,
+        displayReportTime: true,
+        metadata: {
+          browser: {
+            name: browserMap(runInfos.browserName),
+            version: runInfos.browserVersion
+          },
+          device: 'Cypress',
+          platform: {
+            name: osMap(runInfos.osName)
+          }
         },
-        {
-          label: 'Execution Start Time',
-          value: new Date(runInfos.startedTestsAt).toLocaleString()
-        },
-        {
-          label: 'Execution End Time',
-          value: new Date(runInfos.endedTestsAt).toLocaleString()
+        customData: {
+          title: 'Run info',
+          data: [
+            { label: 'Project', value: 'dBildungscloud' },
+            {
+              label: 'Instance',
+              value: runInfos.config.env.BRB || runInfos.config.env.NBC
+            },
+            {
+              label: 'Execution Start Time',
+              value: new Date(runInfos.startedTestsAt).toLocaleString()
+            },
+            {
+              label: 'Execution End Time',
+              value: new Date(runInfos.endedTestsAt).toLocaleString()
+            }
+          ]
         }
-      ]
-    }
-  })
-})
+      })
+    })
+  }
+}
