a53f7c786214aa2b1502ad28b5dd29e0c78752d7
atlldwp <an2478288@gmail.com>
2022-06-22 12:08:01 +0200

    adapted index.js for login file name

--- cypress/support/custom_commands/loginLogout.js
+++ cypress/support/custom_commands/loginLogout.js
@@ -73,9 +73,7 @@ Cypress.Commands.add('login', (username, environment) => {
     cy.get(submitButton).eq(1).click()
     cy.url().should('contain', '/dashboard')
   })
-
   cy.visit('/dashboard')
-
 })
 
 Cypress.Commands.add('logout', () => {
