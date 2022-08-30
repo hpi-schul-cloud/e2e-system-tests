7b65433f7f74e7616c953ea4d122236a48aa9cc0
atlldwp <an2478288@gmail.com>
2022-08-30 14:43:51 +0200

    updated test

--- cypress/support/pages/news/pageNews.js
+++ cypress/support/pages/news/pageNews.js
@@ -1,6 +1,18 @@
 'use strict'
 
 class News {
-  //tbd
+
+  static #pageTitle = '[data-testid="title_of_an_element"]'
+  static #newsText = '[data-testid="body_of_element"]'
+
+  readSchoolNews() {
+    cy.get(News.#pageTitle).contains('test 123')
+    cy.get(News.#newsText).contains('456 lalala')
+  }
+
+  readTeamNews() {
+    cy.get(News.#pageTitle).contains('Musik')
+    cy.get(News.#newsText).contains('Have fun')
+  }
 }
 export default News
