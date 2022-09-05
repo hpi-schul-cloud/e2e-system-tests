7b980d18111dd4bb8958e3f06f5938efef3b0d55
atlldwp <an2478288@gmail.com>
2022-09-05 14:22:54 +0200

    updated the test PR

--- cypress/support/pages/dashboard/pageDashboard.js
+++ cypress/support/pages/dashboard/pageDashboard.js
@@ -6,8 +6,23 @@ class Dashboard {
   static #dashboardTasksTitle = '[data-testid="dashboard-tasks-title"]'
   static #dashboardTaskCourseName = '[data-testid="task-course-name"]'
   static #dashboardTaskName = '[data-testid="task-name"]'
+  static #pageTitle = '[data-testid="title_of_an_element"]'
+  static #newsText = '[data-testid="body_of_element"]'
+  static #newsSection = '[data-testid="news-section"]'
 
-  seeWelcomeMessage() {
+  seeSchoolNews(newsTitle, newsDesc) {
+    cy.get(Dashboard.#newsSection).should('be.visible')
+    cy.get(Dashboard.#pageTitle).contains(newsTitle)
+    cy.get(Dashboard.#newsText).contains(newsDesc)
+  }
+
+  seeTeamsNews(newsTitle, newsDesc) {
+    cy.get(Dashboard.#newsSection).should('be.visible')
+    cy.get(Dashboard.#pageTitle).contains(newsTitle)
+    cy.get(Dashboard.#newsText).contains(newsDesc)
+  }
+
+  seeWelcomeMessage(welcomeMsg) {
     cy.get(Dashboard.#welcomeMessage)
     cy.contains(welcomeMsg)
   }
@@ -16,14 +31,14 @@ class Dashboard {
     cy.get(Dashboard.#dashboardTasksTitle).eq(0)
     cy.contains('Gestellte Aufgaben')
     cy.get(Dashboard.#dashboardTaskCourseName).eq(0).contains('Course with subject and tasks')
-    cy.get(Dashboard.#dashboardTaskName).eq(0).contains('Task11')
+    cy.get(Dashboard.#dashboardTaskName).contains(taskName)
   }
 
   seeDraftTasks(draftName) {
     cy.get(Dashboard.#dashboardTasksTitle).eq(1)
     cy.contains('Entwürfe')
     cy.get(Dashboard.#dashboardTaskCourseName).eq(0).contains('Course with subject and tasks')
-    cy.get(Dashboard.#dashboardTaskName).eq(0).contains('Task1')
+    cy.get(Dashboard.#dashboardTaskName).contains(draftName)
   }
 }
 export default Dashboard
