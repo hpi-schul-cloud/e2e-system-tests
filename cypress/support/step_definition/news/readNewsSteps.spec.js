4bed3349cfd5b24370307e32f4622fbec10f836c
atlldwp <an2478288@gmail.com>
2022-09-05 15:13:43 +0200

    updated read news on overview page

--- cypress/support/step_definition/news/readNewsSteps.spec.js
+++ cypress/support/step_definition/news/readNewsSteps.spec.js
@@ -2,24 +2,11 @@ import News from '../../pages/news/pageNews'
 
 const news = new News()
 
-//Scenario: Reading a school news on the dashboard
-//I am logged in as a 'teacher' at 'brb'
-//step defined -->\step_definition\authentication\loginStep.spec.js
+//loginStep.spec.js
+//commonNewsSteps.spec.js
+//commonTeamsSteps.spec.js
 
-//I go to teams overview
-//step defined --> \step_definition\news\commonNewsRelatedSteps.spec.js
 
-Then('I can read the school news', () => {
-  news.readSchoolNews()
-})
-
-//Scenario: Reading a team news on the dashboard
-//I am logged in as a 'teacher' at 'brb'
-//step defined -->\step_definition\authentication\loginStep.spec.js
-
-//I go to news overview
-//step defined --> \step_definition\teams\commonTeamsRelatedSteps.spec.js
-
-Then('I can read the team news', () => {
-  news.readTeamNews()
+Then('I can read the news {string} with description {string}', (titleOfNews, descriptionOfNews) => {
+  news.teacherReadsNewsOnOverviewPage(titleOfNews, descriptionOfNews)
 })
