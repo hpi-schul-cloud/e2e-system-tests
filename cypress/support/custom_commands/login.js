8a069edb85abaa7931c03f09201e2e12f378ada8
Uzaeir <uzair.khan@dataport.de>
2022-10-10 17:24:28 +0200

    initial commit

--- cypress/support/custom_commands/login.js
+++ cypress/support/custom_commands/login.js
@@ -2,7 +2,8 @@ const emailInputFieldElement = '[data-testid="username-email"]'
 const passwordInputFieldElement = '[data-testid="password-email"]'
 const submitButton = '[data-testid="submit-login-email"]'
 const nbcLoginWithEmailOptionButton = '[data-testid="submit-cloud-site"]'
-const defaultLoginViaExternalBroker = '[data-testid="submit-oauth-login"], [data-provider="oauth"]'
+const defaultLoginViaExternalBroker =
+  '[data-testid="submit-oauth-login"], [data-provider="oauth"]'
 const initials = '[data-testid="initials"]'
 const languageSelection = '[id="selected-language"]'
 const languageDe = '[data-language="de"]'
@@ -17,10 +18,13 @@ Cypress.Commands.add('login', (username, environment) => {
     const link = Cypress.config('baseUrl', env[environmentUpperCased])
     cy.visit(link)
     if (environmentUpperCased === 'NBC') {
-      cy.get(nbcLoginWithEmailOptionButton).eq(1).click()
-    }
-    if (environmentUpperCased === 'DEFAULT') {
-      cy.visit('/login');
+      cy.visit('/login')
+      cy.get(nbcLoginWithEmailOptionButton)
+        .click()
+    } else if (environmentUpperCased === 'DEFAULT') {
+      cy.visit('/login')
+    } else {
+      cy.visit('/login')
     }
 
     let userEmail
@@ -31,48 +35,52 @@ Cypress.Commands.add('login', (username, environment) => {
       case 'teacher':
         userEmail = 'TEACHER_1_EMAIL'
         userPassword = 'TEACHER_1_PASSWORD'
-        break;
+        break
       case 'teacher1':
         userEmail = 'TEACHER_1_EMAIL'
         userPassword = 'TEACHER_1_PASSWORD'
-        break;
+        break
       case 'teacher2':
         userEmail = 'TEACHER_2_EMAIL'
         userPassword = 'TEACHER_2_PASSWORD'
-        break;
+        break
       case 'student':
         userEmail = 'STUDENT_1_EMAIL'
         userPassword = 'STUDENT_1_PASSWORD'
-        break;
+        break
       case 'student1':
         userEmail = 'STUDENT_1_EMAIL'
         userPassword = 'STUDENT_1_PASSWORD'
-        break;
+        break
       case 'student2':
         userEmail = 'STUDENT_2_EMAIL'
         userPassword = 'STUDENT_2_PASSWORD'
-        break;
+        break
       case 'admin':
         userEmail = 'ADMIN_1_EMAIL'
         userPassword = 'ADMIN_1_PASSWORD'
-        break;
+        break
       case 'expert':
         userEmail = 'EXPERT_1_EMAIL'
         userPassword = 'EXPERT_1_PASSWORD'
-        break;
+        break
       case 'student_extern':
         userEmail = 'STUDENT_EXTERN'
         userPassword = 'STUDENT_EXTERN_PASSWORD'
         doExternalLogin = true
-        break;
+        break
     }
-    if (doExternalLogin){
+    if (doExternalLogin) {
       cy.get(defaultLoginViaExternalBroker).click()
-      cy.get(externalUsernameInputFieldElement).type(env[userEmail], {log:false})
-      cy.get(externalPasswordInputFieldElement).type(env[userPassword], {log:false}).type('{enter}')
+      cy.get(externalUsernameInputFieldElement).type(env[userEmail], {
+        log: false
+      })
+      cy.get(externalPasswordInputFieldElement)
+        .type(env[userPassword], { log: false })
+        .type('{enter}')
     } else {
-      cy.get(emailInputFieldElement).type(env[userEmail], {log:false})
-      cy.get(passwordInputFieldElement).type(env[userPassword], {log:false})
+      cy.get(emailInputFieldElement).type(env[userEmail], { log: false })
+      cy.get(passwordInputFieldElement).type(env[userPassword], { log: false })
       cy.get(submitButton).click()
     }
     cy.url().should('contain', '/dashboard')
