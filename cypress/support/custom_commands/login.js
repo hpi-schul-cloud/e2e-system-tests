6acbfb872e3461bf366e1370a350758dc6aed5cd
Uzaeir <97288209+UzaeirKhan@users.noreply.github.com>
2022-10-14 09:28:56 +0200

    Refactor for login  (#75)

--- cypress/support/custom_commands/login.js
+++ cypress/support/custom_commands/login.js
@@ -2,25 +2,30 @@ const emailInputFieldElement = '[data-testid="username-email"]'
 const passwordInputFieldElement = '[data-testid="password-email"]'
 const submitButton = '[data-testid="submit-login-email"]'
 const nbcLoginWithEmailOptionButton = '[data-testid="submit-cloud-site"]'
-const defaultLoginViaExternalBroker = '[data-testid="submit-oauth-login"], [data-provider="oauth"]'
+const defaultLoginViaExternalBroker =
+  '[data-testid="submit-oauth-login"], [data-provider="oauth"]'
 const initials = '[data-testid="initials"]'
 const languageSelection = '[id="selected-language"]'
 const languageDe = '[data-language="de"]'
 
 const externalUsernameInputFieldElement = '[id="Username"]'
 const externalPasswordInputFieldElement = '[id="Password"]'
+const oauth_url =
+  'https://idm-default-main.cd.dbildungscloud.dev/realms/default/protocol/openid-connect/auth?client_id=dbildungscloud-server&redirect_uri=https://default-main.cd.dbildungscloud.dev/api/v3/sso/oauth/62c7f233f35a554ba3ed42f1&response_type=code&scope=openid%20profile%20email&kc_idp_hint=oidcmock'
 
 Cypress.Commands.add('login', (username, environment) => {
   cy.session([username, environment], () => {
     const env = Cypress.env()
     const environmentUpperCased = environment.toUpperCase()
     const link = Cypress.config('baseUrl', env[environmentUpperCased])
-    cy.visit(link)
+    cy.log(link)
     if (environmentUpperCased === 'NBC') {
-      cy.get(nbcLoginWithEmailOptionButton).eq(1).click()
-    }
-    if (environmentUpperCased === 'DEFAULT') {
-      cy.visit('/login');
+      cy.visit('/login')
+      cy.get(nbcLoginWithEmailOptionButton).click()
+    } else if (environmentUpperCased === 'DEFAULT') {
+      cy.visit('/login')
+    } else {
+      cy.visit('/login')
     }
 
     let userEmail
@@ -31,48 +36,58 @@ Cypress.Commands.add('login', (username, environment) => {
       case 'teacher':
         userEmail = 'TEACHER_1_EMAIL'
         userPassword = 'TEACHER_1_PASSWORD'
-        break;
+        break
       case 'teacher1':
         userEmail = 'TEACHER_1_EMAIL'
         userPassword = 'TEACHER_1_PASSWORD'
-        break;
+        break
       case 'teacher2':
         userEmail = 'TEACHER_2_EMAIL'
         userPassword = 'TEACHER_2_PASSWORD'
-        break;
+        break
       case 'student':
         userEmail = 'STUDENT_1_EMAIL'
         userPassword = 'STUDENT_1_PASSWORD'
-        break;
+        break
       case 'student1':
         userEmail = 'STUDENT_1_EMAIL'
         userPassword = 'STUDENT_1_PASSWORD'
-        break;
+        break
       case 'student2':
         userEmail = 'STUDENT_2_EMAIL'
         userPassword = 'STUDENT_2_PASSWORD'
-        break;
+        break
       case 'admin':
         userEmail = 'ADMIN_1_EMAIL'
         userPassword = 'ADMIN_1_PASSWORD'
-        break;
+        break
       case 'expert':
         userEmail = 'EXPERT_1_EMAIL'
         userPassword = 'EXPERT_1_PASSWORD'
-        break;
+        break
       case 'student_extern':
         userEmail = 'STUDENT_EXTERN'
         userPassword = 'STUDENT_EXTERN_PASSWORD'
         doExternalLogin = true
-        break;
+        break
     }
-    if (doExternalLogin){
-      cy.get(defaultLoginViaExternalBroker).click()
-      cy.get(externalUsernameInputFieldElement).type(env[userEmail], {log:false})
-      cy.get(externalPasswordInputFieldElement).type(env[userPassword], {log:false}).type('{enter}')
+    if (doExternalLogin) {
+      cy.request('GET', oauth_url).then(resp => {
+        cy.intercept(resp.requestHeaders.referer).as('oauth_url')
+        cy.visit(resp.requestHeaders.referer)
+        cy.wait('@oauth_url')
+        cy.url().should('include', '/Account/Login')
+        cy.get(externalUsernameInputFieldElement).should('be.visible')
+        cy.get(externalUsernameInputFieldElement).type(env[userEmail], {
+          log: false
+        })
+        cy.get(externalPasswordInputFieldElement)
+          .type(env[userPassword], { log: false })
+          .type('{enter}')
+      })
     } else {
-      cy.get(emailInputFieldElement).type(env[userEmail], {log:false})
-      cy.get(passwordInputFieldElement).type(env[userPassword], {log:false})
+      cy.get(emailInputFieldElement).type(env[userEmail], { log: false })
+      cy.get(passwordInputFieldElement).type(env[userPassword], { log: false })
       cy.get(submitButton).click()
     }
     cy.url().should('contain', '/dashboard')
