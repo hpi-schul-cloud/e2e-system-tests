48059143d132bc973deeea707c05bc6fb59bdb17
Christian Spohr <105202075+csp175@users.noreply.github.com>
2022-11-23 13:19:51 +0100

    Bc 1875 create delete task from taskpage (#81)

--- cypress/support/step_definition/tasks/commonTaskSteps.spec.js
+++ cypress/support/step_definition/tasks/commonTaskSteps.spec.js
@@ -9,3 +9,56 @@ When('I go to tasks overview', () => {
 And('I click on button Submit', () => {
   tasksCommon.clickOnSubmit()
 })
+
+When('I click on button Add Task', () => {
+  tasksCommon.clickOnAddTask()
+})
+
+Then('I can see create task page {string}', (taskTitle) => {
+  tasksCommon.seeCreateTaskPage(taskTitle)
+})
+
+When ('I enter title {string}', (taskTitle) => {
+  tasksCommon.enterTaskTitle(taskTitle)
+})
+
+And ('I enter task description {string}', (taskDescription) => {
+  tasksCommon.setTaskText(taskDescription)
+})
+
+And ('I enter text submission {string}', (submissionText) => {
+  tasksCommon.setTaskText(submissionText)
+})
+
+And ('I enter comment {string}', (gradingText) => {
+  tasksCommon.setTaskText(gradingText)
+})
+
+When('I click on draft tasks tab', () => {
+  tasksCommon.clickOnTabDraftTasks()
+})
+
+Then('I can see task {string} on tasks overview page', taskTitle => {
+  tasksCommon.taskIsVisibleOnTasksOverviewPage(taskTitle)
+})
+
+Then('I can not see task {string} on tasks overview page', taskTitle => {
+  tasksCommon.taskIsNotVisibleOnTasksOverviewPage(taskTitle)
+})
+
+When('I click on three dot menu of task {string}', contentTitle => {
+  tasksCommon.openThreeDotMenuForTask(contentTitle)
+})
+
+When('I click on Delete Task in dot menu', () => {
+  tasksCommon.clickDeleteTaskInDotMenu()
+})
+
+When('I click on task {string} in tasks overview', contentTitle => {
+  tasksCommon.openTaskFromTasksOverview(contentTitle)
+})
+
+When('I click on button Delete', () => {
+  tasksCommon.clickButtonDeleteTask()
+})
+
