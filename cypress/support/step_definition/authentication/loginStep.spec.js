a620860a37a17923d3e759f314f7971eb7755232
Uzaeir <uzair.khan@dataport.de>
2022-11-29 16:45:48 +0100

    Update loginStep.spec.js

--- cypress/support/step_definition/authentication/loginStep.spec.js
+++ cypress/support/step_definition/authentication/loginStep.spec.js
@@ -6,22 +6,9 @@ const {
 } = require('cypress-cucumber-preprocessor/steps')
 
 Before(() => {
-  cy.intercept('**/public').as('public_api')
-  cy.intercept('**/me').as('me_api')
-  cy.intercept('**/user/**').as('roles_api')
-  cy.intercept('**/schools/**').as('schools_api')
-  cy.intercept('**/dashboard').as('dashboard_api')
-  cy.intercept('**/userPermissions?**').as('userPermissions_api')
-  cy.intercept('**/classes?**').as('classes_api')
-  cy.intercept('**/students?**').as('students_api')
-  cy.intercept('**/locales/en.json', req => {
-    delete req.headers['if-none-match']
-  }).as('locales_api')
   cy.intercept('**/alerts', req => {
     delete req.headers['if-none-match']
   }).as('alerts_api')
-  cy.intercept('**/alert').as('alert_api')
-  cy.intercept('**/tasks**').as('tasks_api')
 })
 
 Given('I am logged in as a {string} at {string}', (username, environment) => {
