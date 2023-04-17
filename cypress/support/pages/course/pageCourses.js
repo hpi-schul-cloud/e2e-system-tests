38a873a2a2d0f06e78dae7499bed382467796f2e
Tomasz Wiaderek <t.wiaderwek@gmail.com>
2023-04-17 21:47:02 +0200

    fix tests

--- cypress/support/pages/course/pageCourses.js
+++ cypress/support/pages/course/pageCourses.js
@@ -56,7 +56,7 @@ class Courses {
     cy.get(Courses.#courseOverviewNavigationButton)
         .should('exist')
       .click()
-    cy.wait('@courses_api')
+    cy.wait(['@courses_api', '@alert_api', '@dashboard_api'])
   }
 
   navigateToRoomBoard (roomName) {
