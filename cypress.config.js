8b4543574f407b46d7b9497b846b9fe372b7f19b
Uzaeir Khan <uzair.khan@dataport.de>
2023-11-13 18:03:41 +0100

    BC-5468 update cypress confif and yml file

--- cypress.config.js
+++ cypress.config.js
@@ -5,11 +5,8 @@ const preprocessor = require('@badeball/cypress-cucumber-preprocessor')
 async function setupNodeEvents (on, config) {
   const isCI = config.env.environmentName === 'ci'
   if (isCI) {
-    const environmentFilename = `./env_variables/combined_credentials.json`
-    const settings = require(environmentFilename)
     config.env = {
-      ...config.env,
-      ...settings
+      ...config.env
     }
   } else {
     const environmentName = config.env.environmentName || 'local'
