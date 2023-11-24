1f5b3b7bb3e2e4a94acd8c740e4755e84142e5ec
Uzaeir Khan <uzair.khan@dataport.de>
2023-11-24 03:26:15 +0100

    BC-5468 formatted all files

--- scripts/aggregate-json-files.sh
+++ scripts/aggregate-json-files.sh
@@ -9,49 +9,12 @@ nbc_instance=$4
 workspace_path=$5
 environment=$6
 
-# check_environment() {
-#   local url="$1"
-
-#   if [[ $url == *"staging"* || $url == *"schulportal"* ]]; then
-#     echo "ref"
-#   else
-#     echo "dev"
-#   fi
-# }
-
-# if [[ $workflow_name == *"manual"* ]]; then
-#   echo "This is a manual workflow"
-#   echo "TAG=tag:stable:ci" >>$GITHUB_OUTPUT
-#   echo "$TAG"
-# elif [[ $workflow_name == *"automatic"* || $workflow_name == *"scheduled"* ]]; then
-#   echo "This is an automatic or scheduled workflow"
-#   echo "TAG=tag:stable:ci" >>$GITHUB_OUTPUT
-#   echo "$TAG"
-# else
-#   echo "This is a remote workflow"
-#   echo "TAG=tag:stable:pr:ci" >>$GITHUB_OUTPUT
-#   echo "$TAG"
-# fi
-
-# brb_env=$(check_environment "$brb_instance")
-# dbc_env=$(check_environment "$dbc_instance")
-# nbc_env=$(check_environment "$nbc_instance")
-
-# if [[ $brb_env == "ref" || $dbc_env == "ref" || $nbc_env == "ref" ]]; then
-#   environment="ref"
-# else
-#   environment="dev"
-# fi
-
 file_paths=()
 declare -A key_value_pairs
 
 for instance in "${instances[@]}"; do
-  # if [[ $workflow_name == *"manual"* || $workflow_name == *"remote"* ]]; then
-    file_path="$workspace_path/e2e-system-tests/env_variables/file-${environment}-${instance}.json"
-  # else
-  #   file_path="$workspace_path/e2e-system-tests/env_variables/file-dev-${instance}.json"
-  # fi
+
+  file_path="$workspace_path/e2e-system-tests/env_variables/file-${environment}-${instance}.json"
 
   file_paths+=("$file_path")
 
