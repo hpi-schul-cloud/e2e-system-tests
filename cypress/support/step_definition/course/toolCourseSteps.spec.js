5ad4b98c63c5b992e768830467633e148239bd4f
Malte Berg <malte.berg@capgemini.com>
2023-11-29 15:42:30 +0100

    add test

--- cypress/support/step_definition/course/toolCourseSteps.spec.js
+++ cypress/support/step_definition/course/toolCourseSteps.spec.js
@@ -4,32 +4,36 @@ import Courses from '../../pages/course/pageCourses'
 const courses = new Courses()
 
 When('I click on the tools tab', () => {
-	courses.navigateToToolsTab()
+    courses.navigateToToolsTab()
 })
 
 Then('I can see the button to add a tool', () => {
-	courses.seeAddNewToolFAB()
+    courses.seeAddNewToolFAB()
 })
 
 Then('I cant see the button to add a tool', () => {
-	courses.seeNotAddNewToolFAB()
+    courses.seeNotAddNewToolFAB()
 })
 
 When('I click on the button to add a tool', () => {
-	courses.clickOnAddNewToolFAB()
+    courses.clickOnAddNewToolFAB()
 })
 
 Then('I can see the tool configuration page title', () => {
-	courses.seeContextExternalToolConfiguratorPageTitle()
+    courses.seeContextExternalToolConfiguratorPageTitle()
 })
 
 
 When('I click on the tool configuration selection', () => {
-	courses.clickOnToolConfigurationSelect()
+    courses.clickOnToolConfigurationSelect()
 })
 
 Then('I can enter {string} as tool name in the selection', toolName => {
-	courses.enterAnToolNameInToolConfigurationSelect(toolName)
+    courses.enterAnToolNameInToolConfigurationSelect(toolName)
+})
+
+Then('I can not see tool {string} in the tool selection list', toolName => {
+    courses.checkIfToolIsVisible(toolName)
 })
 
 
