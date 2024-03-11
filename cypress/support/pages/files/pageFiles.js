732af2c86ee2e424c139e83bd9ac8703c153649f
Uzaeir Khan <uzair.khan@dataport.de>
2024-03-11 14:20:30 +0100

    BC-6643 update method in pageFiles file

--- cypress/support/pages/files/pageFiles.js
+++ cypress/support/pages/files/pageFiles.js
@@ -1,170 +1,166 @@
 'use strict'
 
 class Files {
-  static #newFile = '[data-testid="create-new-file-btn"]'
-  static #filetypeDropdown = '#file_ending_chosen'
-  static #filetypeDocument = 'li.active-result:nth-child(2)'
-  static #filetypeTable = 'li.active-result:nth-child(3)'
-  static #filetypePresentation = 'li.active-result:nth-child(4)'
-  static #filenameInputField = '#file-name'
-  static #newFilenameInputField = '[data-testid="folder-rename-text-field"]'
-  static #createFileButtonOnModal = '[data-testid="btn-submit-Neue Datei erstellen"]'
-  static #downloadFile = '[data-testid="file-download-btn"]'
-  static #renameFile = '[data-testid="file-edit-btn"]'
-  static #saveRenameFile = '[data-testid="submit-btn-rename-modal"]'
-  static #deleteFile = '[data-testid="file-delete-btn"]'
-  static #confirmDeleteFile = '[data-testid="delete-files-btn"]'
-  static #shareFile = '[data-testid="file-share-btn"]'
-  static #moveFile = '[data-testid="file-move-btn"]'
-  static #filePermissionsFile = '[data-testid="file-settings-btn"]'
-  static #cardTitle = '[data-testid="file-title"]'
-  static #pageTitle = '[data-testid="LibreOffice Online"]'
-  static #deleteDialogBoxPopupContainer = '[data-testid="modal_content"]'
-  static #filesOverviewNavigationButton = '[data-testid="Meine Dateien"]'
-  static #personalFilesOverviewNavigationButton =
-    '[data-testid="persönliche Dateien"]'
-  static #coursesFilesOverviewNavigationButton = '[data-testid="Kurse"]'
-  static #teamsFilesOverviewNavigationButton = '[data-testid="Teams"]'
-  static #sharedFilesOverviewNavigationButton =
-    '[data-testid="geteilte Dateien"]'
-  static #editFilePopupBoxTitle = '[data-testid="popup-title"]'
-  static #editFilePopupBoxTextField = '[data-testid="folder-rename-text-field"]'
+	static #newFile = '[data-testid="create-new-file-btn"]'
+	static #filetypeDropdown = '#file_ending_chosen'
+	static #filetypeDocument = 'li.active-result:nth-child(2)'
+	static #filetypeTable = 'li.active-result:nth-child(3)'
+	static #filetypePresentation = 'li.active-result:nth-child(4)'
+	static #filenameInputField = '#file-name'
+	static #newFilenameInputField = '[data-testid="folder-rename-text-field"]'
+	static #createFileButtonOnModal =
+		'[data-testid="btn-submit-Neue Datei erstellen"]'
+	static #downloadFile = '[data-testid="file-download-btn"]'
+	static #renameFile = '[data-testid="file-edit-btn"]'
+	static #saveRenameFile = '[data-testid="submit-btn-rename-modal"]'
+	static #deleteFile = '[data-testid="file-delete-btn"]'
+	static #confirmDeleteFile = '[data-testid="delete-files-btn"]'
+	static #shareFile = '[data-testid="file-share-btn"]'
+	static #moveFile = '[data-testid="file-move-btn"]'
+	static #filePermissionsFile = '[data-testid="file-settings-btn"]'
+	static #cardTitle = '[data-testid="file-title"]'
+	static #pageTitle = '[data-testid="LibreOffice Online"]'
+	static #deleteDialogBoxPopupContainer = '[data-testid="modal_content"]'
+	static #filesOverviewNavigationButton = '[data-testid="Meine Dateien"]'
+	static #personalFilesOverviewNavigationButton =
+		'[data-testid="persönliche Dateien"]'
+	static #coursesFilesOverviewNavigationButton = '[data-testid="Kurse"]'
+	static #teamsFilesOverviewNavigationButton = '[data-testid="Teams"]'
+	static #sharedFilesOverviewNavigationButton =
+		'[data-testid="geteilte Dateien"]'
+	static #editFilePopupBoxTitle = '[data-testid="popup-title"]'
+	static #editFilePopupBoxTextField = '[data-testid="folder-rename-text-field"]'
 
-  static #testAssertionData = {
-    fileTypeDocument: 'Textdokument (docx)',
-    libraOfficeOpenTitleText: 'LibreOffice Online'
-  }
+	static #testAssertionData = {
+		fileTypeDocument: 'Textdokument (docx)',
+		libraOfficeOpenTitleText: 'LibreOffice Online'
+	}
 
-  navigateToFilesOverview () {
-    cy.get(Files.#filesOverviewNavigationButton).click()
-    cy.wait('@alerts_api')
-    cy.url().should('include', '/files')
-  }
+	navigateToFilesOverview () {
+		cy.get(Files.#filesOverviewNavigationButton).click()
+		cy.wait('@alerts_api')
+		cy.url().should('include', '/files')
+	}
 
-  navigateToPersonalFilesOverview () {
-    cy.get(Files.#personalFilesOverviewNavigationButton).click()
-    cy.wait('@alerts_api')
-    cy.url().should('include', '/files/my')
-  }
+	navigateToPersonalFilesOverview () {
+		cy.get(Files.#personalFilesOverviewNavigationButton).click()
+		cy.wait('@alerts_api')
+		cy.url().should('include', '/files/my')
+	}
 
-  navigateToCourseFilesOverview () {
-    cy.get(Files.#coursesFilesOverviewNavigationButton).click()
-    cy.wait('@alerts_api')
-    cy.url().should('include', '/files/courses')
-  }
+	navigateToCourseFilesOverview () {
+		cy.get(Files.#coursesFilesOverviewNavigationButton).click()
+		cy.wait('@alerts_api')
+		cy.url().should('include', '/files/courses')
+	}
 
-  navigateToTeamsFilesOverview () {
-    cy.get(Files.#teamsFilesOverviewNavigationButton).eq(1).click()
-    cy.wait('@alerts_api')
-    cy.url().should('include', '/files/teams')
-  }
+	navigateToTeamsFilesOverview () {
+		cy.get(Files.#teamsFilesOverviewNavigationButton).eq(1).click()
+		cy.wait('@alerts_api')
+		cy.url().should('include', '/files/teams')
+	}
 
-  navigateToSharedFilesOverview () {
-    cy.get(Files.#sharedFilesOverviewNavigationButton).click()
-    cy.wait('@alerts_api')
-    cy.url().should('include', '/files/shared')
-  }
+	navigateToSharedFilesOverview () {
+		cy.get(Files.#sharedFilesOverviewNavigationButton).click()
+		cy.wait('@alerts_api')
+		cy.url().should('include', '/files/shared')
+	}
 
-  clickOnCreateNewFile () {
-    cy.get(Files.#newFile).click()
-  }
+	clickOnCreateNewFile () {
+		cy.get(Files.#newFile).click()
+	}
 
-  selectFiletypeDocument () {
-    cy.get(Files.#filetypeDropdown).click()
-    cy.get(Files.#filetypeDocument)
-      .contains(Files.#testAssertionData.fileTypeDocument)
-      .should('be.visible')
-      .click()
-  }
+	selectFiletypeDocument () {
+		cy.get(Files.#filetypeDropdown).click()
+		cy.get(Files.#filetypeDocument)
+			.contains(Files.#testAssertionData.fileTypeDocument)
+			.should('be.visible')
+			.click()
+	}
 
-  typeFilename (fileName) {
-    cy.get(Files.#filenameInputField)
-      .type(fileName)
-      .should('have.value', fileName)
-  }
+	typeFilename (fileName) {
+		cy.get(Files.#filenameInputField)
+			.type(fileName)
+			.wait(500)
+			.should('have.value', fileName)
+	}
 
-  clickOnCreateFile () {
-    cy.get(Files.#createFileButtonOnModal)
-      .click()
-  }
+	clickOnCreateFile () {
+		cy.get(Files.#createFileButtonOnModal).click()
+	}
 
-  clickOnFileWithName (fileName) {
-    cy.get(Files.#cardTitle).contains(fileName).should('be.visible').click()
-  }
+	clickOnFileWithName (fileName) {
+		cy.get(Files.#cardTitle).contains(fileName).should('be.visible').click()
+	}
 
-  clickOnRenameFile (fileName) {
-    cy.get(Files.#cardTitle)
-      .contains(fileName)
-      .then(() => {
-        cy.get(Files.#renameFile).first().click()
-      })
-  }
+	clickOnRenameFile (fileName) {
+		cy.get(Files.#cardTitle)
+			.contains(fileName)
+			.then(() => {
+				cy.get(Files.#renameFile).first().click()
+			})
+	}
 
-  renamePopupBoxVisible (fileName) {
-    cy.get(Files.#editFilePopupBoxTextField)
-      .should('be.visible')
-      .should('have.value', fileName)
-  }
+	renamePopupBoxVisible (fileName) {
+		cy.get(Files.#editFilePopupBoxTextField)
+			.should('be.visible')
+			.should('have.value', fileName)
+	}
 
-  typeNewFilename (fileName) {
-    cy.get(Files.#newFilenameInputField)
-      .focus()
-      .clear()
-      .type(fileName, { force: true })
-      .should('have.value', fileName)
-  }
+	typeNewFilename (fileName) {
+		cy.get(Files.#newFilenameInputField)
+			.focus()
+			.clear()
+			.type(fileName, { force: true })
+			.should('have.value', fileName)
+	}
 
-  clickOnSaveFilename () {
-    cy.get(Files.#saveRenameFile)
-      .click()
-    cy.wait('@alerts_api')
-  }
+	clickOnSaveFilename () {
+		cy.get(Files.#saveRenameFile).click()
+		cy.wait('@alerts_api')
+	}
 
-  clickOnDeleteFile (fileName) {
-    cy.get(Files.#cardTitle)
-      .contains(fileName)
-      .then(() => {
-        cy.get(Files.#deleteFile).first().should('be.visible').click()
-        cy.get(Files.#deleteDialogBoxPopupContainer).should('be.visible')
-      })
-  }
+	clickOnDeleteFile (fileName) {
+		cy.get(Files.#cardTitle)
+			.contains(fileName)
+			.then(() => {
+				cy.get(Files.#deleteFile).first().should('be.visible').click()
+				cy.get(Files.#deleteDialogBoxPopupContainer).should('be.visible')
+			})
+	}
 
-  clickOnConfirmDeleteFileOnModal () {
-    cy.get(Files.#confirmDeleteFile)
-      .focus()
-      .click()
-  }
+	clickOnConfirmDeleteFileOnModal () {
+		cy.get(Files.#confirmDeleteFile).focus().click()
+	}
 
-  libreOfficeOpens () {
-    cy.url().should('include', '/files/file/')
-    cy.wait('@alerts_api')
-    cy.contains(
-      Files.#pageTitle,
-      Files.#testAssertionData.libraOfficeOpenTitleText
-    ).should('be.visible')
-  }
+	libreOfficeOpens () {
+		cy.url().should('include', '/files/file/')
+		cy.wait('@alerts_api')
+		cy.contains(
+			Files.#pageTitle,
+			Files.#testAssertionData.libraOfficeOpenTitleText
+		).should('be.visible')
+	}
 
-  fileNameIsShown (fileName) {
-    cy.get(Files.#cardTitle)
-      .should('contain', fileName)
-  }
+	fileNameIsShown (fileName) {
+		cy.get(Files.#cardTitle).should('contain', fileName)
+	}
 
-  fileNameIsNotShown (fileName) {
-    cy.contains(fileName)
-      .should('not.exist')
-  }
+	fileNameIsNotShown (fileName) {
+		cy.contains(fileName).should('not.exist')
+	}
 
-  //doesn't work
-  enterTextIntoDocument (text) {
-    cy.wait(600)
-    cy.get('.leaflet-layer').type(text, { force: true })
-  }
+	//doesn't work
+	enterTextIntoDocument (text) {
+		cy.wait(600)
+		cy.get('.leaflet-layer').type(text, { force: true })
+	}
 
-  //doesn't work
-  textIsShownInDocument (text) {
-    cy.intercept('/lool/*').as('loleaflet')
-    cy.wait('@loleaflet')
-    cy.get('iframe').should('contain', text)
-  }
+	//doesn't work
+	textIsShownInDocument (text) {
+		cy.intercept('/lool/*').as('loleaflet')
+		cy.wait('@loleaflet')
+		cy.get('iframe').should('contain', text)
+	}
 }
 export default Files
