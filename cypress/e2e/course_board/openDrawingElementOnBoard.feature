4948a6f3356ae5f871ddfa2cb52b0ab47e39609b
Uzaeir Khan <uzair.khan@dataport.de>
2024-05-13 12:13:25 +0200

    BC-7287 initial commit

--- cypress/e2e/course_board/openDrawingElementOnBoard.feature
+++ cypress/e2e/course_board/openDrawingElementOnBoard.feature
@@ -1,12 +1,56 @@
 @pr
 @release
+@stable_test
 Feature: Course Board - Opening a drawing element on a course page
 
     As a teacher and student I want to open already created drawing element on a course page
 
-    @stable_test
-    Scenario Outline:  Teacher is able to open a drawing element to the course board
+    Scenario: Pre-test: Creating all users
+        Given I am logged in as a 'admin1_nbc' at 'nbc'
+        Given I am logged in as a 'teacher1_nbc' at 'nbc'
+        Given I am logged in as a 'student1_nbc' at 'nbc'
 
+    Scenario: Pre-test: Creating new course
+        Given I am logged in as a 'admin1_nbc' at 'nbc'
+        When I go to rooms overview
+        When I click on FAB to create a new room
+        When I click on new course create button in sub menu
+        Then I see section one area on the course create page
+        When I enter the course title 'German'
+        When I select room colour as red
+        Then I select 'cypress teacher_1' from field teacher
+        Then I see substitute teacher selection box
+        Then I see button to create a course time table container
+        When I click on button Next Steps after entering the room detail in section one
+        Then I see section two area on the course create page
+        When I select 'cypress student_1' from field student
+        When I click on button Next Steps after selecting room participant details
+        Then I see the section three area as the finish page
+        When I click on button To Course Overview on the finish page
+    Scenario: Pre-Test: Teacher create course board, add whiteboard and publish the board
+        Given I am logged in as a 'teacher1_nbc' at 'nbc'
+        When I go to rooms overview
+        When I go to room 'German'
+        When I go to the tab contents in course detail page
+        When I click on FAB to create new content
+        When I click on the button FAB New Column Board
+        Then I see the page Course Board detail
+        When I click on the button three dot menu in course board
+        When I click on the option Edit in three dot menu in course board
+        Then I enter the course board title 'Board Cy Title'
+        When I click on the page outside of the column
+        Then I see the chip Draft in the course board
+        When I click on the button three dot menu in course board
+        Then I click on the option Publish in three dot menu in course board
+
+        # Teacher adds a new column ---
+        When I click on the button Add column in the course board
+        When I click on the page outside of the column
+        When I click on plus icon to add card in column
+        When I click on plus icon to add content into card
+        Then I select whiteboard from the menu
+
+    Scenario:  Teacher is able to open a drawing element to the course board
         # Teacher open the drawing element to the course column
         Given I am logged in as a 'teacher1_nbc' at 'nbc'
         When I go to rooms overview
@@ -14,11 +58,9 @@ Feature: Course Board - Opening a drawing element on a course page
         When I click on card Course Board
         When I click on open Drawing Element
 
-    @stable_test
-    Scenario Outline:  Student is able to open a drawing element to the course board
-
+    Scenario:  Student is able to open a drawing element to the course board
         # Student open the drawing element to the course column
-        Given I am logged in as a 'student2_nbc' at 'nbc'
+        Given I am logged in as a 'student1_nbc' at 'nbc'
         When I go to rooms overview
         When I go to room 'German'
         When I click on card Course Board
--- cypress/support/pages/course_board/pageBoard.js
+++ cypress/support/pages/course_board/pageBoard.js
@@ -2,7 +2,7 @@
 
 class Board {
 	static #courseContentTab = '[data-testid="learnContent-tab"]';
-	static #cardCourseBoardInCouseContent = '[data-testid="room-board-card-0"]';
+	static #cardCourseBoardInCourseContent = '[data-testid="room-board-card-0"]';
 	static #courseBoardTitleOnPage = '[data-testid="board-title"]';
 	static #welcomeDefaultCardInColumn = '[data-testid="event-handle"]';
 	static #addNewColumnButton = '[data-testid="add-column"]';
@@ -14,16 +14,36 @@ class Board {
 	static #deleteOptionColumnThreeDot = '[data-testid="board-menu-action-delete"]';
 	static #columnDeleteButtonInModal = '[data-testid="dialog-confirm"]';
 	static #deleteDialogBox = '[data-testid="dialog-title"]';
-	static #drawingElement = '.drawing-element-content';
+	static #drawingElement = '[data-testid="drawing-element"]';
 	static #columnPlaceholder = '[placeholder="Spalte 1"]';
-	static #newColumnBoardFABInCOurseDetail = '[data-testid="fab_button_add_board"]';
+	static #newColumnBoardFABInCourseDetail = '[data-testid="fab_button_add_board"]';
 	static #threeDotInCourseBoardTitle = '[data-testid="board-menu-icon"]';
 	static #editOptionInThreeDotCourseBoardTitle = '[data-testid="board-menu-action-edit"]';
 	static #courseBoardTitle = '[data-testid="board-title"]';
 	static #draftChipInCourseBoardName = '[data-testid="board-title"]';
+	static #addCardInColumnButton = '[data-testid="column-0-add-card-btn"]';
+	static #addContentIntoCardButton = '[data-testid="add-element-btn"]';
+	static #selectWhiteboardFromMenu = '[data-testid="create-element-drawing-element"]';
+	static #boardMenuActionPublish = '[data-testid="board-menu-action-publish"]';
+
+	clickPlusIconToAddCardInColumn() {
+		cy.get(Board.#addCardInColumnButton).click();
+	}
+
+	clickPlusIconToAddContentIntoCard() {
+		cy.get(Board.#addContentIntoCardButton).click();
+	}
+
+	selectWhiteboardFromMenu() {
+		cy.get(Board.#selectWhiteboardFromMenu).click();
+	}
+
+	clickPublishOptionInThreeDotMenuInCourseBoard() {
+		cy.get(Board.#boardMenuActionPublish).click();
+	}
 
 	clickOnFABToCreateNewColumnBoard() {
-		cy.get(Board.#newColumnBoardFABInCOurseDetail).click();
+		cy.get(Board.#newColumnBoardFABInCourseDetail).click();
 	}
 
 	seeNewCourseBoardCreatePage() {
@@ -55,7 +75,10 @@ class Board {
 	}
 
 	clickOnOpenTldrawDrawingElement() {
-		cy.get(Board.#drawingElement).should('exist').click();
+		cy.get(Board.#drawingElement).should("exist").click();
+		cy.wait("@tldrawRequest").then((interception) => {
+			assert.isTrue(interception.response.statusCode === 200, "Status code is 200");
+		});
 	}
 
 	clickOnDeleteColumnModal() {
@@ -72,6 +95,7 @@ class Board {
 
 	clickOnThreeDotOnColumn() {
 		cy.get(Board.#threeDotMenuInColumn).click();
+		cy.get(Board.#editOptionColumnThreeDot).should("be.visible");
 	}
 
 	selectEditInColumnThreeDotMenu() {
@@ -83,7 +107,7 @@ class Board {
 	}
 
 	clickOnCourseBoardCardInCourseDetailPage() {
-		cy.get(Board.#cardCourseBoardInCouseContent).click();
+		cy.get(Board.#cardCourseBoardInCourseContent).click();
 	}
 
 	seeCourseBoardTitle() {
@@ -96,42 +120,42 @@ class Board {
 
 	clickOnAddNewColumnButton() {
 		cy.get(Board.#addNewColumnButton).click();
+		cy.get(Board.#addColumnTitleInput).should("be.visible").should("exist");
 	}
 
 	enterColumnTitleInCourseBoard(columnTitle) {
 		cy.get(Board.#addColumnTitleInput).type("{backspace}".repeat(30));
+		cy.wait(500);
 		cy.get(Board.#addColumnTitleInput).wait(1000).realType(columnTitle).wait(500);
 	}
 
-	/* Commented out because it's being handled in the same another method -> enterColumnTitleInCourseBoard()
-	recursivelyDeleteTextFromTextArea() {
-		cy.window().then((win) => {
-			const textArea = win.document.querySelector(Board.#columnPlaceholder);
-			const textAreaValue = textArea.value;
+	//  Commented out because it's being handled in the same another method -> enterColumnTitleInCourseBoard()
+	// recursivelyDeleteTextFromTextArea() {
+	// 	cy.window().then((win) => {
+	// 		const textArea = win.document.querySelector(Board.#columnPlaceholder);
+	// 		const textAreaValue = textArea.value;
 
-			if (textAreaValue.length > 0) {
-				cy.get(Board.#addColumnTitleInput)
-					.wait(100)
-					.realType(`{backspace}`)
-					.wait(100)
-					.then(() => {
-						textArea.value = textAreaValue.slice(0, -1);
-					});
-				this.recursivelyDeleteTextFromTextArea();
-			} else {
-				cy.log(`Cleared text area`);
-			}
-		});
-	}
-	*/
+	// 		if (textAreaValue.length > 0) {
+	// 			cy.get(Board.#addColumnTitleInput)
+	// 				.wait(100)
+	// 				.realType(`{backspace}`)
+	// 				.wait(100)
+	// 				.then(() => {
+	// 					textArea.value = textAreaValue.slice(0, -1);
+	// 				});
+	// 			this.recursivelyDeleteTextFromTextArea();
+	// 		} else {
+	// 			cy.log(`Cleared text area`);
+	// 		}
+	// 	});
+	// }
 
-	/* Commented out because it's being handled in the same another method -> enterColumnTitleInCourseBoard()
-	enterEditedColumnTitle(newColumnName) {
-		this.recursivelyDeleteTextFromTextArea();
-		cy.wait(500);
-		cy.get(Board.#addColumnTitleInput).wait(1000).realType(newColumnName).wait(500);
-	}
-	*/
+	// Commented out because it's being handled in the same another method -> enterColumnTitleInCourseBoard()
+	// enterEditedColumnTitle(newColumnName) {
+	// 	this.recursivelyDeleteTextFromTextArea();
+	// 	cy.wait(500);
+	// 	cy.get(Board.#addColumnTitleInput).wait(1000).realType(newColumnName).wait(500);
+	// }
 
 	clickOutsideTheColumnToSaveTheColumn() {
 		cy.get(Board.#mainPageArea).click("center");
