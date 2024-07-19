bca6591e945581c483d885af2e8e7cc470ac2408
Mrika Llabani <mrika.llabani@capgemini.com>
2024-07-19 08:47:04 +0200

    N21-1995 adjust + fix tests

--- cypress/support/pages/admin/pageAdministration.js
+++ cypress/support/pages/admin/pageAdministration.js
@@ -201,7 +201,7 @@ class Management {
 	}
 
 	navigateToNewClassAdministration() {
-		cy.get(Management.#newClassAdministrationNavigationButton).click();
+		cy.get(Management.#classAdministrationNavigationButton).click();
 		cy.url().should("include", "/administration/groups/classes");
 	}
 
