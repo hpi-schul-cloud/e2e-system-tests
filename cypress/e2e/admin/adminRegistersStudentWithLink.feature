987838af89201a822280ad1a5329ddf4f1fcdf02
atlldwp <an2478288@gmail.com>
2024-10-16 15:18:55 +0200

    refactoring tests

--- cypress/e2e/admin/adminRegistersStudentWithLink.feature
+++ cypress/e2e/admin/adminRegistersStudentWithLink.feature
@@ -1,38 +1,58 @@
 @regression_test
-@unstable_test
+@stable_test
 Feature: Student registration with registration link send by admin
 
     As a student, I want to register in the SVS
 
-    Scenario Outline: Setup
-        Given I am logged in as '<user>' on '<env>'
-        When Created student 'John' 'Student' with email 'john.student@mail.tld'
-        Then I am on the students management page
+    Scenario: Admin add a new student and register it via registration link
 
+        # pre-condition: admin logs in to create their account in a school
+        Given I am logged in as a '<admin>' at '<namespace>'
+
+        # Admin creates a new student
+        When I click on administration in menu
+        When I go to '<role_to_manage>' administration
+        When I click on FAB
+        When I click on Add User in opened FAB for '<role_to_manage>'
+        When I fill out the user creation form for '<user_firstname>' '<user_lastname>' with email '<user_email>'
+        When I click on add button to add '<role_to_manage>'
+        When I enter '<role_to_manage>' email '<user_email>' in search input field
+        Then I can see the user with email '<user_email>' in the table
+
+        # Admin generates the registeration link to proceed with student registration
+        When I enter '<role_to_manage>' email '<user_email>' in search input field
+        When I click edit '<role_to_manage>' button for '<user_email>'
+        Then I click on the button Generate Personal Registration Link
+
+        # Student performs registration via the generated link and do the first login
+        Then I visit to the generated student registration link
+        When I choose the language for the registration process
+        Then I click on the button Next to proceed to the age selection
+        When I select the age over 16 years for registration
+        Then I click on the button Next to proceed to the personal data information page
+        Then I see my first name
+        Then I see my last name
+        When I click on the button Next to proceed to the next step
+        When I accept the privacy and terms of use consents
+        Then I click on the button Next to proceed to the registration pin step
+        When I request a new registration pin
+        Then I retrieve the registration pin to enter it into the form
+        Then I click on the button Send and Get Started to successfully complete the registration process
+        Then I see the page user data summary
+
+        # Admin deletes the newly added student
+        Given I am logged in as a '<admin>' at '<namespace>'
+        When I click on administration in menu
+        When I go to '<role_to_manage>' administration
+        When I enter '<role_to_manage>' email '<user_email>' in search input field
+        When I click edit '<role_to_manage>' button for '<user_email>'
+        When I click delete user button to delete user with lastname '<user_lastname>'
+        When I click on delete button in pop up
+        When I enter '<role_to_manage>' email '<user_email>' in search input field
+        Then I can not see user '<user_email>' in the table
+
+        @school_api_test
         Examples:
-            | env | user       |
-            | dbc | admin1_dbc |
-
-    Scenario Outline: Student can registers in the SVS with the registration link
-        Given I am logged in as '<user>' on '<env>'
-        And I am on the students management page
-        Given Going to student edit page for 'John' with email 'john.student@mail.tld'
-        And Going to student registration page
-        And I am registering as student with email 'john.student@mail.tld' on '<env>'
-        Then I am able to logout
-
-        Examples:
-            | env | user       |
-            | dbc | admin1_dbc |
-
-    Scenario Outline: Teardown
-        Given I am logged in as '<user>' on '<env>'
-        And I am on the students management page
-        And Going to student edit page for 'John' with email 'john.student@mail.tld'
-        When Deleting the user
-        Then I am on the students management page
-
-        Examples:
-            | env | user       |
-            | dbc | admin1_dbc |
+            | namespace | admin      | role_to_manage | user_firstname | user_lastname     | user_email                                  |
+            | dbc       | admin1_dbc | student        | cypress        | student_admintest | original_student_adminusers@cypress-mail.de |
 
--- cypress/support/step_definition/admin/adminRegistersStudentWithLinkSteps.spec.js
+++ cypress/support/step_definition/admin/adminRegistersStudentWithLinkSteps.spec.js
@@ -1,57 +1,97 @@
 import { defineStep } from "@badeball/cypress-cucumber-preprocessor";
 import { getPageUrl } from "../../custom_commands/input.utils";
 
-defineStep("Going to student registration page", () => {
-	cy.clickOnElement("button[class*='btn-invitation-link-with-hash student']");
+const { When, Then } = require("@badeball/cypress-cucumber-preprocessor");
+import Management from "../../pages/admin/pageAdministration";
+
+const management = new Management();
+
+Then("I click on the button Generate Personal Registration Link", () => {
+	//management.clickOnGeneratePersonalRegistrationLink()
+	cy.get("button[class*='btn-invitation-link-with-hash student']").click();
+});
+
+Then("I visit to the generated student registration link", () => {
+	//management.visitToStudentRegistrationPage()
 	cy.get("input[id='invitation-link']")
 		.should("be.visible")
 		.invoke("val")
 		.then((val) => {
+			expect(val).to.be.a("string").and.not.to.be.empty;
 			cy.clearCookies();
 			cy.visit(val);
 		});
 });
 
-defineStep(
-	"I am registering as student with email {string} on {string}",
-	(email, environment) => {
-		// Choosing language
-		cy.get("div[id='language_chosen']")
-			.clickOnElement()
-			.contains("li", "deutsch", { matchCase: false })
-			.clickOnElement();
-		cy.clickOnElement("button[id='showAgeSelection']");
+When("I choose the language for the registration process", () => {
+	// management.chooseLanguageOnRegistrationProcess()
+	cy.get("div[id='language_chosen']").click(); // Open the language dropdown
+	cy.contains("li", "deutsch", { matchCase: false }).click(); // Select 'Deutsch' from the list
+});
 
-		// Selecting age
-		cy.clickOnElement("input[id='reg-16']");
-		cy.clickOnElement("button[id='showRegistrationForm']");
+Then("I click on the button Next to proceed to the age selection", () => {
+	cy.get("button[id='showAgeSelection']").click(); // Click to proceed to the age selection step
+});
 
-		// Confirming information
-		cy.clickOnElement("button[id='nextSection']");
+When("I select the age over 16 years for registration", () => {
+	// management.selectAgeOnRegistrationProcess()
+	cy.get("input[id='reg-16']").click(); // Select the appropriate age group (16+)
+});
 
-		// Accepting consents only for DBC environment
-		if (Cypress.config().baseUrl.includes("dbc")) {
-			cy.clickOnElement("input[name='privacyConsent']");
-			cy.clickOnElement("input[name='termsOfUseConsent']");
-			cy.clickOnElement("button[id='nextSection']");
-		}
-
-		// Entering registration pin
-		cy.clickOnElement("button[id='resend-pin']");
-		cy.get("div[class*='alert-success']").should("be.visible");
-		cy.request({
-			method: "GET",
-			headers: {
-				"X-API-KEY": Cypress.env(`apiKey-${environment}`),
-			},
-			url: getPageUrl(environment, `/admin/api/v1/registration-pin/${email}`),
-		}).then(({ body }) => {
-			const pin = body.pop().registrationPin.split("");
-
-			cy.get("div[id='pinverification'] input[class='digit']").each((el, index) => {
-				cy.wrap(el).type(pin[index]);
-			});
-		});
-		cy.clickOnElement("button[id='nextSection']");
+Then(
+	"I click on the button Next to proceed to the personal data information page",
+	() => {
+		//management.clickOnNextButtonToNextStep()
+		cy.get("button[id='showRegistrationForm']").click(); // Click to open the registration form
 	}
 );
+
+When("I click on the button Next to proceed to the next step", () => {
+	cy.get("button[id='nextSection']").click(); // Click the button to confirm the registration information
+});
+
+When("I accept the privacy and terms of use consents", () => {
+	//management.acceptingConsentOnRegistrationProcess()
+	if (Cypress.config().baseUrl.includes("dbc")) {
+		cy.get("input[name='privacyConsent']").click(); // Accept privacy consent
+		cy.get("input[name='termsOfUseConsent']").click(); // Accept terms of use consent
+	}
+});
+
+Then("I click on the button Next to proceed to the registration pin step", () => {
+	cy.get("button[id='nextSection']").click(); // Proceed to the next section
+});
+
+When("I request a new registration pin", () => {
+	cy.get("button[id='resend-pin']").click(); // Request a new registration pin
+	cy.get("div[class*='alert-success']").should("be.visible"); // Confirm that the pin request was successful
+});
+
+Then("I retrieve the registration pin to enter it into the form", () => {
+	//management.retrieveAndEnterRegistrationCode()
+	cy.request({
+		method: "GET",
+		headers: {
+			"X-API-KEY": Cypress.env(`apiKey-${environment}`),
+		},
+		url: getPageUrl(environment, `/admin/api/v1/registration-pin/${email}`),
+	}).then(({ body }) => {
+		expect(body).to.be.an("array").that.is.not.empty; // Ensure the response contains the pin data
+		const pin = body.pop().registrationPin.split("");
+
+		cy.get("div[id='pinverification'] input[class='digit']").each((el, index) => {
+			cy.wrap(el).type(pin[index]); // Enter each digit of the pin into the form
+		});
+	});
+});
+
+Then(
+	"I click on the button Send and Get Started to successfully complete the registration process",
+	() => {
+		cy.get("button[id='nextSection']").click(); // Finalize the registration process
+	}
+);
+
+Then("I see the page user data summary", () => {
+	cy.get((id = "userdata-summary")).should("be.visible");
+});
