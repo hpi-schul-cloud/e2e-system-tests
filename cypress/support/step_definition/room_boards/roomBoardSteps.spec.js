9fa1ff0502cca84ff6928512bade9b2ab9e70c3a
wolfganggreschus <wolfgang@greschus.de>
2024-12-11 10:28:10 +0100

    Refactor room board feature tests by removing outdated scenarios and implementing new multi-column board steps

---
+++ cypress/support/step_definition/room_boards/roomBoardSteps.spec.js
@@ -0,0 +1,58 @@
+const { When, Then } = require("@badeball/cypress-cucumber-preprocessor");
+import RoomBoards from "../../pages/room_board/pageRoomBoards";
+
+const roomBoards = new RoomBoards();
+
+
+Then("I see the column board dialog box", () => {
+	roomBoards.seeColumnBoardDialogBox();
+});
+
+When("I click on button to add multi column board", () => {
+	roomBoards.clickOnButtonToAddMultiColumnBoard();
+});
+
+Then("I see the page room board details", () => {
+	roomBoards.seeNewRoomBoardCreatePage();
+});
+
+When("I click on the button three dot menu in room board", () => {
+	roomBoards.clickOnThreeDotMenuInRoomBoardTitle();
+});
+
+When("I click on edit in board menu", () => {
+	roomBoards.clickOnEditInBoardMenu();
+});
+
+Then("I enter the room board title {string}", (boardTitle) => {
+	roomBoards.enterRoomBoardTitle(boardTitle);
+});
+
+When("I click on the page outside of the title of the board", () => {
+	roomBoards.clickOutsideTheTitleToSaveTheModifiedTitle();
+});
+Then("I see my room board is named {string}", (boardTitle) => {
+	roomBoards.seeGivenRoomBoardTitle(boardTitle);
+});
+When("I click on delete in board menu", () => {
+	roomBoards.clickOnDeleteInBoardMenu();
+});
+Then("I see the button to cancel the dialog", () => {
+	roomBoards.seeBtnDialogCancel();
+});
+When("I click on the button to cancel the deletion", () => {
+	roomBoards.clickOnBtnDialogCancel();
+});
+Then("I see the board {string} on the room overview page", (boardTitle) => {
+	roomBoards.boardIsVisibleOnOverviewPage(boardTitle);
+});
+Then("I see the button to confirm the dialog", () => {
+	roomBoards.seeBtnDialogConfirmDelete();
+});
+When("I click on the button to confirm the deletion", () => {
+	roomBoards.clickBtnDialogConfirmDelete();
+});
+Then("I do not see the board {string} in the room", (boardTitle) => {
+	roomBoards.boardIsNotVisibleOnOverviewPage(boardTitle);
+});
+
