fca3f4189407720625d86f4605ee22f07198cf1e
atlldwp <an2478288@gmail.com>
2025-01-16 09:08:59 +0100

    copy board in room test

---
+++ cypress/e2e/room_board/copyColumnAndListBoardInRoom.feature
@@ -0,0 +1,30 @@
+@regression_test
+@stable_test
+Feature: Room - Add, remove BBB tool in the room board
+
+    As a teacher I want to add and remove BBB tool in the room board so that I can manage video confernce.
+
+    Scenario: Teacher add and remove BBB tool in the room board, including pre-conditions
+        Given I am logged in as a '<teacher>' at '<namespace>'
+        Given a room with name '<room_name>' exist
+        Given a multicolumn board with the name '<edited_board_title>' exists in the room
+        Given a list board with the name '<edited_board_title>' exists in the room
+
+        # teacher copies column board
+
+        # teacher copies list board
+
+        # post-condition: teacher deletes room
+        Given room with name '<room_name>' is deleted
+
+
+
+        @school_api_test
+        Examples:
+            | teacher      | namespace | room_name         | board_name         | edited_board_title |
+            | teacher1_brb | brb       | Cypress Room Name | Cypress Board Name | Board Cy Title     |
+
+        @staging_test
+        Examples:
+            | teacher      | namespace | room_name         | board_name         | edited_board_title |
+            | teacher1_brb | brb       | Cypress Room Name | Cypress Board Name | Board Cy Title     |
--- cypress/support/step_definition/common_helper/prePostConditionSteps.spec.js
+++ cypress/support/step_definition/common_helper/prePostConditionSteps.spec.js
@@ -1,20 +1,78 @@
 import { Given, Then, When } from "@badeball/cypress-cucumber-preprocessor";
 import Board from "../../pages/course_board/pageBoard";
 import Courses from "../../pages/course/pageCourses";
+import RoomBoards from "../../pages/room_board/pageRoomBoards";
+import Rooms from "../../pages/room/pageRooms";
 
+const roomBoards = new RoomBoards();
+const rooms = new Rooms();
 const courses = new Courses();
 const board = new Board();
 
-Given("a course with name {string} exists with {string} as teacher and {string} as student", (courseName, teacherName, studentName) => {
-	courses.navigateToCoursesOverview();
-	courses.clickOnCreateCourseFAB();
-	courses.fillCourseCreationForm(courseName);
-	courses.selectCourseColour();
-	courses.selectTeacherInCourseCreatePage(teacherName);
-	courses.clickOnNextStepsBtnAfterEnteringCourseDetails();
-	courses.selectStudentInCourseCreatePage(studentName);
-	courses.clickOnNextStepButtonOnCourseParticipationDetail();
+Given("room with name {string} is deleted", (room_name) => {
+	rooms.openThreeDotMenuForRoom();
+	rooms.openDeleteInThreeDotMenuForRoom();
+	rooms.seeConfirmationModalForRoomDeletion();
+	rooms.clickDeleteInConfirmationModal();
+	rooms.roomIsNotVisibleOnOverviewPage(room_name);
 });
+
+Given("a room with name {string} exist", (room_name) => {
+	rooms.navigateToRoomsOverview();
+	rooms.clickOnCreateRoomFAB();
+	rooms.showRoomCreationPage();
+	rooms.fillRoomFormName(room_name);
+	rooms.selectRoomColour();
+	rooms.selectTodayStartDateForRoom();
+	rooms.selectEndDateForRoom();
+	rooms.submitRoom();
+	rooms.seeRoomDetailPage(newRoomName);
+});
+
+Given(
+	"a multicolumn board with the name {string} exists in the room",
+	(edited_board_title) => {
+		rooms.clickOnAddContentButton();
+		rooms.seeFabButtonToAddBoard();
+		rooms.clickOnFabButtonToAddBoard();
+		roomBoards.seeColumnBoardDialogBox();
+		roomBoards.clickOnButtonToAddMultiColumnBoard();
+		roomBoards.seeNewRoomBoardCreatePage();
+		roomBoards.clickOnThreeDotMenuInRoomBoardTitle();
+		roomBoards.clickOnEditInBoardMenu();
+		roomBoards.enterRoomBoardTitle(edited_board_title);
+		roomBoards.clickOutsideTheTitleToSaveTheModifiedTitle();
+		roomBoards.seeGivenRoomBoardTitle(edited_board_title);
+	}
+);
+
+Given("a list board with the name {string} exists in the room", (edited_board_title) => {
+	rooms.clickOnAddContentButton();
+	rooms.seeFabButtonToAddBoard();
+	rooms.clickOnFabButtonToAddBoard();
+	roomBoards.seeColumnBoardDialogBox();
+	roomBoards.clickOnButtonToAddSingleColumnBoard();
+	roomBoards.seeNewRoomBoardCreatePage();
+	roomBoards.clickOnThreeDotMenuInRoomBoardTitle();
+	roomBoards.clickOnEditInBoardMenu();
+	roomBoards.enterRoomBoardTitle(edited_board_title);
+	roomBoards.clickOutsideTheTitleToSaveTheModifiedTitle();
+	roomBoards.seeGivenRoomBoardTitle(edited_board_title);
+});
+
+Given(
+	"a course with name {string} exists with {string} as teacher and {string} as student",
+	(courseName, teacherName, studentName) => {
+		courses.navigateToCoursesOverview();
+		courses.clickOnCreateCourseFAB();
+		courses.fillCourseCreationForm(courseName);
+		courses.selectCourseColour();
+		courses.selectTeacherInCourseCreatePage(teacherName);
+		courses.clickOnNextStepsBtnAfterEnteringCourseDetails();
+		courses.selectStudentInCourseCreatePage(studentName);
+		courses.clickOnNextStepButtonOnCourseParticipationDetail();
+	}
+);
 
 Given("a board exists in course {string}", (courseName) => {
 	courses.navigateToCoursesOverview();
