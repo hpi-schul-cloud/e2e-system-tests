f97d5031133239700eb695c055fea85ed08305da
Pooja-Sutrave <pooja.sutravedataport@gmail.com>
2025-10-17 17:44:27 +0200

    Update cypress/support/pages/common_helper/globalAssertions.js
    Co-authored-by: Uzaeir <97288209+UzaeirKhan@users.noreply.github.com>

--- cypress/support/pages/common_helper/globalAssertions.js
+++ cypress/support/pages/common_helper/globalAssertions.js
@@ -68,7 +68,8 @@ class GlobalAssertions {
 			infoPointsArray.map((point, index) => {
 				const infoPointers = point.replace(/\s+/g, "-");
 				// First item = info section, others = modal section
-				const selector =
+				const section = index === 0 ? "info" : "modal";
+				return [point, `[data-testid="${modalType}-${section}-${infoPointers}"]`];
 					index === 0
 						? `[data-testid="${modalType}-info-${infoPointers}"]`
 						: `[data-testid="${modalType}-modal-${infoPointers}"]`;
