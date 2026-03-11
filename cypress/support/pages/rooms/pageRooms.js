5fa3d68ae9b7125f99a794e6d1f918b51f52177a
Uzaeir Khan <uzair.khan@dataport.de>
2026-03-11 11:40:28 +0100

    BC-11451: following files has been updated:
    - shareAndImportCardWithDifferentSchoolOnStaging.feature - resolves permission issue and old step in post condition
    - pageAccount.js - resolves account visibility checkbox issue in pre condition
    - pageRooms.js - possible fix for browser crash

--- cypress/support/pages/rooms/pageRooms.js
+++ cypress/support/pages/rooms/pageRooms.js
@@ -730,6 +730,7 @@ class Rooms {
 
 	clickOnSpeedDialOption(option) {
 		cy.get(`[data-testid="fab-${option}-icon-btn"]`).should("be.visible").click();
+		cy.wait(500);
 	}
 }
 export default Rooms;
