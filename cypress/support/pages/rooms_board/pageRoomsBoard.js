642f29de884a9f54e43a47e1325325088b25949d
atlldwp <an2478288@gmail.com>
2026-03-19 16:12:41 +0100

    added testid fix

--- cypress/support/pages/rooms_board/pageRoomsBoard.js
+++ cypress/support/pages/rooms_board/pageRoomsBoard.js
@@ -127,6 +127,7 @@ class RoomBoards {
 	static #moveCardSelectRoom = '[data-testid="move-card-select-room"]';
 	static #moveCardSelectColumn = '[data-testid="move-card-select-column"]';
 	static #confirmButtonOnModal = '[data-testid="rename-folder-dialog-confirm"]';
+	static #confirmationButtonDialog = '[data-testid="confirmation-dialog-confirm"]';
 	static #globalDialogConfirmButton = '[data-testid="confirm-dialog-confirm"]';
 	static #importCardDialogConfirm = '[data-testid="import-card-dialog-confirm"]';
 	static #globalDialogTitle = '[data-testid="confirm-dialog-title"]';
@@ -890,7 +891,12 @@ class RoomBoards {
 	}
 
 	clickDeleteButtonInConfirmationDialog() {
-		cy.get(RoomBoards.#globalDialogConfirmButton).click();
+		cy.get(
+			`${RoomBoards.#globalDialogConfirmButton}, ${RoomBoards.#confirmationButtonDialog}`
+		)
+			.filter(":visible")
+			.first()
+			.click();
 		cy.wait(1000);
 		// Refresh the page to let the UI re-render properly in case of some external tools like Etherpad.
 		cy.reload();
@@ -1050,21 +1056,43 @@ class RoomBoards {
 	doNotSeeBoardOnRoomDetailPage(boardName) {
 		cy.contains(boardName).should("not.exist");
 	}
+
 	seeBtnDialogCancel() {
 		cy.get(RoomBoards.#btnDialogCancel).should("be.visible");
 	}
+
 	clickOnBtnDialogCancel() {
 		cy.get(RoomBoards.#btnDialogCancel).click();
 	}
+
 	seeBtnDialogConfirmDelete() {
-		cy.get(RoomBoards.#globalDialogConfirmButton).should("be.visible");
+		cy.get("body").then(($body) => {
+			if ($body.find(RoomBoards.#globalDialogConfirmButton).length > 0) {
+				cy.get(RoomBoards.#globalDialogConfirmButton).should("be.visible");
+			} else if ($body.find(RoomBoards.#confirmationButtonDialog).length > 0) {
+				cy.get(RoomBoards.#confirmationButtonDialog).should("be.visible");
+			} else {
+				throw new Error("No confirm delete button found in dialog.");
+			}
+		});
 	}
+
 	clickBtnDialogConfirmDelete() {
-		cy.get(RoomBoards.#globalDialogConfirmButton).click();
+		cy.get("body").then(($body) => {
+			if ($body.find(RoomBoards.#globalDialogConfirmButton).length > 0) {
+				cy.get(RoomBoards.#globalDialogConfirmButton).click();
+			} else if ($body.find(RoomBoards.#confirmationButtonDialog).length > 0) {
+				cy.get(RoomBoards.#confirmationButtonDialog).click();
+			} else {
+				throw new Error("No confirm delete button found in dialog.");
+			}
+		});
 	}
+
 	clickOnThreeDotMenuInRoomBoardTitle() {
 		cy.get(RoomBoards.#threeDotInBoardTitle).click();
 	}
+
 	clickOnEditInBoardMenu() {
 		cy.get(RoomBoards.#btnBoardMenuActionRename).click();
 	}
