eaa4c625826df50e39a90d64a3043326a8c84d5c
atlldwp <an2478288@gmail.com>
2026-07-01 16:51:34 +0200

    added new feature add new card from card

--- cypress/support/pages/rooms_board/pageRoomsBoard.js
+++ cypress/support/pages/rooms_board/pageRoomsBoard.js
@@ -171,6 +171,37 @@ class RoomBoards {
 	static #checkboxImportRoomList = 'input[type="checkbox"]';
 	static #listboxRoomsSelection = 'div[role="listbox"]';
 
+	verifyCardTitleInBoard(cardTitle) {
+		cy.get(RoomBoards.#inputCardTitle)
+			.should("be.visible")
+			.and("contain.text", cardTitle);
+	}
+
+	verifyCardTitleNotInBoard(cardTitle) {
+		cy.get(RoomBoards.#inputCardTitle)
+			.filter(":visible")
+			.should(($titles) => {
+				const cardTitles = [...$titles].map((title) => title.textContent.trim());
+				expect(cardTitles).not.to.include(cardTitle);
+			});
+	}
+
+	verifyCardInsertedAboveCard(newCardTitle, existingCardTitle) {
+		cy.get(RoomBoards.#inputCardTitle)
+			// use all visible titles to avoid hidden elements that may be present in the DOM but not visible to the user
+			.filter(":visible")
+			.should(($titles) => {
+				const cardTitles = [...$titles].map((title) => title.textContent.trim());
+
+				expect(cardTitles).to.include(newCardTitle);
+				expect(cardTitles).to.include(existingCardTitle);
+				// a smaller index means the new card appears above the existing card.
+				expect(cardTitles.indexOf(newCardTitle)).to.be.lessThan(
+					cardTitles.indexOf(existingCardTitle)
+				);
+			});
+	}
+
 	selectTwoRoomsForBoardImport(roomName1, roomName2) {
 		cy.get(
 			`${RoomBoards.#dialogTitle}, ${RoomBoards.#selectDestinationModalTitle}`
@@ -355,7 +386,13 @@ class RoomBoards {
 	}
 
 	enterCardTitleInBoard(cardTitle) {
-		cy.get(RoomBoards.#inputCardTitle).should("be.visible").clear().type(cardTitle);
+		cy.wait(500);
+		cy.get(RoomBoards.#inputCardTitle)
+			.filter(":visible")
+			.find("textarea, input")
+			.first()
+			.click()
+			.type(cardTitle);
 	}
 
 	selectRoomInImportModal(roomName) {
